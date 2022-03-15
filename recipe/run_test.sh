64b70c78463f6cc600b78455d6168f8e59d71986
Kai Tietz <ktietz@anaconda.com>
2022-03-15 09:56:54 +0100

    update to 0.55.1

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -9,29 +9,22 @@ export PYTHONFAULTHANDLER=1
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
   SEGVCATCH=catchsegv
+  export CC="${CC} -pthread"
 elif [[ "$unamestr" == 'Darwin' ]]; then
   SEGVCATCH=""
 else
   echo Error
 fi
 
-# On my computer, on Windows:
-# (Intel(R) Xeon(R) CPU E5-2697 v3 @ 2.60GHz * 2, 56 threads total)
-# 56: Ran 9901 tests in 1944.480s
-# 12: Ran 9901 tests in 1953.651s
-#  8: Ran 9901 tests in 2116.660s
-#  4: Ran 9901 tests in 2559.670s
-if [[ ${CPU_COUNT} > 12 ]]; then
-  TEST_NPROCS=12
-else
-  TEST_NRPOCS=${CPU_COUNT}
-fi
-
 # limit CPUs in use on PPC64LE, fork() issues
 # occur on high core count systems
-archstr=$(uname -m)
+archstr=`uname -m`
 if [[ "$archstr" == 'ppc64le' ]]; then
-  TEST_NPROCS=1
+    TEST_NPROCS=1
+elif [[ "$archstr" == 'aarch64' ]]; then
+    TEST_NPROCS=4
+else
+    TEST_NPROCS=${CPU_COUNT}
 fi
 
 # Check Numba executables are there
@@ -46,7 +39,7 @@ python -m numba.tests.test_runtests
 
 if [[ "$archstr" == 'aarch64' ]]; then
 	echo 'Running only a slice of tests'
-	$SEGVCATCH python -m numba.runtests -b -j --random='0.20' --exclude-tags='long_running' -m $TEST_NPROCS -- numba.tests
+	$SEGVCATCH python -m numba.runtests -b -j --random='0.15' --exclude-tags='long_running' -m $TEST_NPROCS -- numba.tests
 # For now, skip tests on ppc64le because of known errors in the testing suite on ppc64le https://github.com/numba/numba/issues/4026
 elif [[ "$archstr" == 'ppc64le' ]]; then
 	echo 'Skipping tests on ppc64le for testing'
