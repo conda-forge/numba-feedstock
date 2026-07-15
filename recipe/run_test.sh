f66b74b8229456c571cbba37df65a2ba972f14b4
mementorc <claude.rc@gmail.com>
2026-07-15 08:40:59 -0500

    (ref) no-build-isolation

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -40,7 +40,7 @@ else
     # of low accuracy SVML libm replacements in ufunc loops.
     _NPY_CMD='from numba.misc import numba_sysinfo;\
               sysinfo=numba_sysinfo.get_sysinfo();\
-              print("AVX512_SKX" in sysinfo.get("NumPy Supported SIMD features", ()) and
+              print("AVX512_SKX" in sysinfo.get("NumPy Supported SIMD dispatch", ()) and
                     sysinfo.get("NumPy Version", "0")>="1.22")'
     NUMPY_DETECTS_AVX512_SKX_NP_GT_122=$(python -c "$_NPY_CMD")
     echo "NumPy >= 1.22 with AVX512_SKX detected: $NUMPY_DETECTS_AVX512_SKX_NP_GT_122"
